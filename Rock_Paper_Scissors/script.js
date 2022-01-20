afea9b7599fe9b1e9d27f79773a0e0816fdad94b
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-01-20 10:03:52 +0100

    Add a randombackgroundcolor generator button + change the reset button function

--- Rock_Paper_Scissors/script.js
+++ Rock_Paper_Scissors/script.js
@@ -9,6 +9,7 @@ const weaponArray = ['Rock', 'Paper', 'Scissors'];
 const random = Math.floor(Math.random() * weaponArray.length);
 const userScore = document.getElementById("userScore");
 const pcScore = document.getElementById("pcScore");
+const changeBgcolor = document.getElementById("changeBgcolor");
 let oppScore = 0;
 let myScore = 0;
 
@@ -44,43 +45,43 @@ for (let weapon of weaponz.children) {
 const play = (event) => {
     const randomWeapon = weaponArray[Math.floor(Math.random() * weaponArray.length)];
     message2.innerHTML = "Opponent's weapon: " + randomWeapon;
-    if(randomWeapon == "Paper" && message1.innerHTML == "Your weapon: Rock") {
-        oppScore+= 1;
+    if (randomWeapon == "Paper" && message1.innerHTML == "Your weapon: Rock") {
+        oppScore += 1;
         pcScore.innerHTML = "Opp. Score: " + oppScore
         message3.innerHTML = "YOU LOSE"
     }
-    if(randomWeapon == "Scissors" && message1.innerHTML == "Your weapon: Paper") {
-        oppScore+= 1;
+    if (randomWeapon == "Scissors" && message1.innerHTML == "Your weapon: Paper") {
+        oppScore += 1;
         pcScore.innerHTML = "Opp. Score: " + oppScore
         message3.innerHTML = "YOU LOSE"
     }
-    if(randomWeapon == "Rock" && message1.innerHTML == "Your weapon: Scissors") {
-        oppScore+= 1;
+    if (randomWeapon == "Rock" && message1.innerHTML == "Your weapon: Scissors") {
+        oppScore += 1;
         pcScore.innerHTML = "Opp. Score: " + oppScore
         message3.innerHTML = "YOU LOSE"
     }
-    if(randomWeapon == "Rock" && message1.innerHTML == "Your weapon: Paper") {
-        myScore+= 1;
+    if (randomWeapon == "Rock" && message1.innerHTML == "Your weapon: Paper") {
+        myScore += 1;
         userScore.innerHTML = "Score: " + myScore
         message3.innerHTML = "YOU WIN"
     }
-    if(randomWeapon == "Paper" && message1.innerHTML == "Your weapon: Scissors") {
-        myScore+= 1;
+    if (randomWeapon == "Paper" && message1.innerHTML == "Your weapon: Scissors") {
+        myScore += 1;
         userScore.innerHTML = "Score: " + myScore
         message3.innerHTML = "YOU WIN"
     }
-    if(randomWeapon == "Scissors" && message1.innerHTML == "Your weapon: Rock") {
-        myScore+= 1;
+    if (randomWeapon == "Scissors" && message1.innerHTML == "Your weapon: Rock") {
+        myScore += 1;
         userScore.innerHTML = "Score: " + myScore
         message3.innerHTML = "YOU WIN"
     }
-    if(randomWeapon == "Rock" && message1.innerHTML == "Your weapon: Rock") {
+    if (randomWeapon == "Rock" && message1.innerHTML == "Your weapon: Rock") {
         message3.innerHTML = "DRAW!"
     }
-    if(randomWeapon == "Paper" && message1.innerHTML == "Your weapon: Paper") {
+    if (randomWeapon == "Paper" && message1.innerHTML == "Your weapon: Paper") {
         message3.innerHTML = "DRAW!"
     }
-    if(randomWeapon == "Scissors" && message1.innerHTML == "Your weapon: Scissors") {
+    if (randomWeapon == "Scissors" && message1.innerHTML == "Your weapon: Scissors") {
         message3.innerHTML = "DRAW!"
     }
 }
@@ -88,9 +89,27 @@ const play = (event) => {
 const playButton = document.getElementById("playButton");
 playButton.addEventListener("click", play);
 
-const reset = (event) => {
-    location.reload();
+const reset = () => {
+    oppScore = 0
+    myScore = 0
+    message1.innerHTML = "Your weapon: ";
+    message2.innerHTML = "Opponent's weapon: "
+    message3.innerHTML = ""
+    rock.style.backgroundColor = "white";
+    paper.style.backgroundColor = "white";
+    scissors.style.backgroundColor = "white";
+    userScore.innerHTML = "Score: " + myScore ;
+    pcScore.innerHTML = "Opp. Score: " + oppScore ;
 }
 
 const resetButton = document.getElementById("resetButton");
 resetButton.addEventListener("click", reset);
+
+const setBg = () => {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    const randomColor2 = Math.floor(Math.random() * 16777215).toString(16);
+    const randomDegree = Math.floor(Math.random() * 360) + "deg"; 
+    document.body.style.background = "linear-gradient(" + randomDegree + ", #" + randomColor + ", #" + randomColor2 + ")";
+}
+
+changeBgcolor.addEventListener("click", setBg);
